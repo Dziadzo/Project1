18567840ff4360df99e6bad596214a70455ff516
kononenko_a@itstep.academy <kononenko_a@itstep.academy>
2021-08-29 18:59:03 +0300

    ok

--- Project1/Header.h
+++ Project1/Header.h
@@ -3,3 +3,9 @@
 #include <iostream>;
 #include <iomanip>;
 using namespace std;
+
+int** createArray(int m, int n);
+void pushRandomNumber(int** array, int m, int n);
+void printArray(int** array, int m, int n);
+void delArray(int** array, int m, int n);
+void checkArray(int** array, int m, int n);
